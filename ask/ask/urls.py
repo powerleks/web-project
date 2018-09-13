6ee6091d546a57da09e0b248cf7abb647a9cd669
powerleks <powerleks@yandex.ru>
2018-09-13 14:11:21 +0300

    added django

---
+++ ask/ask/urls.py
@@ -0,0 +1,27 @@
+"""ask URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from qa import views
+
+urlpatterns = [
+    url(r'^$', views.test, name='index'),
+    url(r'^login/$', views.test, name='login'),
+    url(r'^signup/$', views.test, name='signup'),
+    url(r'^question/\d+$', views.test, name='ask'),
+    url(r'^ask/$', views.test, name='ask'),
+    url(r'^popular/$', views.test, name='popular'),
+    url(r'^new/$', views.test, name='new'),
+]
