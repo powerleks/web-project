4e7b96928ba1e2bb07c052f99d59a26e1609a4f9
powerleks <powerleks@yandex.ru>
2018-09-13 14:22:36 +0300

    updated

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/test_d
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf   /etc/gunicorn.d/test_d
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start﻿
